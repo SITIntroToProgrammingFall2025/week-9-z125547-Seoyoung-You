c6d59c017524b97ac9e2bcb3974e95da08914c73
z125547-Seoyoung-You <z125547@sic.shibaura-it.ac.jp>
2025-12-09 22:48:39 +0900

    Update ahw9-2.c

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int arr[ARRAY_SIZE];
+    int i, j;     
+    int temp;       
+
+    for (i = 0; i < ARRAY_SIZE; i++) {
+        if (scanf("%d", &arr[i]) != 1) {
+            return 1;
+        }
+    }
+
+    for (i = 0; i < ARRAY_SIZE - 1; i++) {
+        for (j = 0; j < ARRAY_SIZE - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("sorted array:\n");
+    for (i = 0; i < ARRAY_SIZE; i++) {
+        printf("%d", arr[i]);
+        if (i < ARRAY_SIZE - 1) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
 
