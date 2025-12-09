a25c9ceec9fe08054bf61fa3ae0cbcf9f2c81be1
z125547-Seoyoung-You <z125547@sic.shibaura-it.ac.jp>
2025-12-09 22:39:51 +0900

    Update hw9-4.c

--- hw9-4.c
+++ hw9-4.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int r, g, b;
+
+    if (scanf("%d %d %d", &r, &g, &b) != 3) {
+        return 1;
+    }
+
+    printf("The hex code is #%02X%02X%02X\n", r, g, b);
+
+    return 0;
+}
 
